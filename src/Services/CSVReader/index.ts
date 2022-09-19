defe296f74180e339e9b6b11b3933d187b6773ce
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-09-19 14:48:34 -0300

    feat: implement csv reader

---
+++ src/Services/CSVReader/index.ts
@@ -0,0 +1,41 @@
+import { CsvError, parse } from 'csv-parse'
+import fs from 'fs'
+
+
+class CSVReader {
+
+  /**
+   * It reads a file, parses it, and then calls a callback function with the parsed data
+   * @param {string} filePath - The path to the file you want to read.
+   * @param callback - (err: CsvError | undefined, data: any) => void
+   */
+  // eslint-disable-next-line no-unused-vars
+  readFile(filePath: string, callback: (err: CsvError | undefined, data: any) => void): null | void {
+    const fileData = fs.readFileSync(filePath)
+
+    if (fileData) parse(fileData, { delimiter: ';' }, callback)
+  }
+
+
+  /**
+   * It checks if the column schema and the csv header are the same length, and if they are, it checks
+   * if the ordering of the columns is the same
+   * @param {string[]} columnSchema - The column schema that you want to validate against.
+   * @param {string[]} csvHeader - The header of the CSV file.
+   * @returns A boolean value
+   */
+  validateSchema(columnSchema: string[], csvHeader: string[]) {
+    if (columnSchema.length !== csvHeader.length) return false
+
+    let isOrderingValid
+
+    csvHeader.forEach((column, index) => {
+      if (columnSchema[index] !== column) isOrderingValid = false
+      else isOrderingValid = true
+    })
+
+    return isOrderingValid
+  }
+}
+
+export default new CSVReader();
---
+++ src/Tests/Services/csvReader.spec.ts
@@ -0,0 +1,58 @@
+import CSVReader from '@Services/CSVReader'
+import fs from 'fs';
+
+
+describe('CSVReader Service', () => {
+  it('should validate schemas', () => {
+    const schema = ['data', 'movie', 'timestamp']
+
+    const validation = CSVReader.validateSchema(schema, schema)
+
+    expect(validation).toBe(true)
+  });
+
+  it('should not validate different schemas if lengths are different', () => {
+    const schema = ['data', 'movie', 'timestamp']
+    const otherSchema = ['data', 'movie']
+
+    const validation = CSVReader.validateSchema(schema, otherSchema)
+
+    expect(validation).toBe(false)
+  });
+  it('should not validate different schemas if the headers order are different', () => {
+    const schema = ['data', 'movie', 'timestamp']
+    const otherSchema = ['data', 'timestamp', 'movie']
+
+    const validation = CSVReader.validateSchema(schema, otherSchema)
+
+    expect(validation).toBe(false)
+  });
+
+  it('should read a csv file and call callback', () => {
+    const filePath = 'MOCKED_PATH'
+    const mockedCallback = jest.fn()
+    const mockedFs = jest.fn()
+
+    jest.spyOn(fs, 'readFileSync')
+      .mockImplementation(mockedFs)
+
+    CSVReader.readFile(filePath, mockedCallback)
+
+    expect(mockedFs).toBeCalled()
+    expect(mockedFs).toHaveBeenCalledWith(filePath)
+  });
+  it('readFile method should equal undefined when there is no file', () => {
+    const filePath = 'MOCKED_PATH'
+    const mockedCallback = jest.fn()
+    const mockedFs = jest.fn()
+
+    jest.spyOn(fs, 'readFileSync')
+      .mockImplementation(mockedFs)
+
+    const reader = CSVReader.readFile(filePath, mockedCallback)
+
+    expect(mockedFs).toBeCalled()
+    expect(mockedFs).toHaveBeenCalledWith(filePath)
+    expect(reader).not.toBeDefined()
+  });
+})
