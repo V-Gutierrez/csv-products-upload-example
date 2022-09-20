c71445fbeae6c69b6704f895b91ffc215e8545b6
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-09-20 11:58:56 -0300

    chore: add pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: `lm-test-backend`,
+      script: './dist/index.js',
+      instances: '4',
+      exec_mode: 'cluster',
+      node_args: '--optimize_for_size --max_old_space_size=460 --gc_interval=100',
+      env: {
+        NODE_ENV: 'localhost',
+      },
+      env_development: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_staging: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+    },
+  ],
+}
